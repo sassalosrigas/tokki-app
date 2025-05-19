df39e8b80c35d4bb92dbf9c94e5ce87b3e3163be
tsaranto <119889560+tsaranto@users.noreply.github.com>
2025-05-20 00:19:38 +0300

    central cee

--- app/src/main/java/com/example/tokki/ManagerMain.java
+++ app/src/main/java/com/example/tokki/ManagerMain.java
@@ -1,17 +1,27 @@
 package com.example.tokki;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class ManagerMain extends AppCompatActivity {
+    View popupView;
+    AlertDialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +34,39 @@ public class ManagerMain extends AppCompatActivity {
             return insets;
         });
 
+
         findViewById(R.id.addstore_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ManagerMain.this, "Entering Add Store", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(ManagerMain.this, AddStorePage.class);
-                startActivity(intent);
+                LayoutInflater inflater = getLayoutInflater();
+                View popupView = inflater.inflate(R.layout.popup_add_store, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(ManagerMain.this);
+                builder.setView(popupView);
+
+                // Show the dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                MaterialButton addBtn = popupView.findViewById(R.id.addBtn);
+                MaterialButton cancelBtn = popupView.findViewById(R.id.cancelBtn);
+                TextInputEditText input = popupView.findViewById(R.id.storeNameEditText);
+
+                // Add button behavior
+                addBtn.setOnClickListener(bv -> {
+                    String storeName = input.getText().toString();
+                    // Do something with the input
+                    Toast.makeText(ManagerMain.this, "Store added: " + storeName, Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                });
+
+                // Cancel button behavior
+                cancelBtn.setOnClickListener(bv -> dialog.dismiss());
             }
         });
+
+
 
         findViewById(R.id.addproduct_button).setOnClickListener(new View.OnClickListener() {
             @Override
