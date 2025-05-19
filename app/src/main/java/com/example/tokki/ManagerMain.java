4567fe5d9935f58b82d878c70955e5cae39d255d
tsaranto <119889560+tsaranto@users.noreply.github.com>
2025-05-19 22:25:13 +0300

    akoma ligotero asximo manager KAI me koumpakia

--- app/src/main/java/com/example/tokki/ManagerMain.java
+++ app/src/main/java/com/example/tokki/ManagerMain.java
@@ -1,6 +1,9 @@
 package com.example.tokki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,51 @@ public class ManagerMain extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.addstore_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ManagerMain.this, "Entering Add Store", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ManagerMain.this, AddStorePage.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.addproduct_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ManagerMain.this, "Entering Add Product", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ManagerMain.this, AddProductPage.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.removeproduct_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ManagerMain.this, "Entering Remove Product", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ManagerMain.this, RemoveProductPage.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.modifystock_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ManagerMain.this, "Entering Modify Stock", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ManagerMain.this, ModifyStockPage.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.stats_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ManagerMain.this, "Entering Statistics", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ManagerMain.this, StatisticsPage.class);
+                startActivity(intent);
+            }
         });
     }
 }
