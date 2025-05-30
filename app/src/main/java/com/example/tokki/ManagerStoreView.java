d3383580a6c48fc05db4879ab012cf97cee1c1a0
rigas <sassalosrigas@gmail.com>
2025-05-30 19:21:58 +0300

    category_item.xml

--- app/src/main/java/com/example/tokki/ManagerStoreView.java
+++ app/src/main/java/com/example/tokki/ManagerStoreView.java
@@ -105,16 +105,20 @@ public class ManagerStoreView extends AppCompatActivity{
                 intent.putExtra("STORE", clickedStore);
                 intent.putExtra("FUNCTION", "SALES_PER_PRODUCT");
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_down);
             }else if (function.equals("SALES_PER_PRODUCT_CATEGORY")){
                 Intent intent = new Intent(ManagerStoreView.this, ProductView.class);
                 intent.putExtra("STORE", clickedStore);
                 intent.putExtra("FUNCTION", "SALES_PER_PRODUCT_CATEGORY");
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_down);
+            }else if (function.equals("SALES_PER_STORE_CATEGORY")){
+                Intent intent = new Intent(ManagerStoreView.this, ProductView.class);
+                intent.putExtra("STORE", clickedStore);
+                intent.putExtra("FUNCTION", "SALES_PER_STORE_CATEGORY");
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_down);
             }
-
         });
-
     }
-
 }
